dc3dc992233e1b9b5b0c1215ddfbb5ebcaa3df47
Tony Knudson <tk.91.09@gmail.com>
2024-01-12 09:30:35 -0600

    added index functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,11 @@
 class ArticlesController < ApplicationController
 
   def show
-    debugger
+    # debugger
     @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
   end
 end
