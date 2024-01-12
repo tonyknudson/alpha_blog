74a4da56aefe16052f906dd7903a12ac5a9b0fb3
Tony Knudson <tonyknudson91@gmail.com>
2024-01-12 14:37:38 -0600

    added error handling and success message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,18 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
