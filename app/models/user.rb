61c941fb106ff55db970bf122ee23d2311febd94
Tony Knudson <tk.91.09@gmail.com>
2024-01-13 12:30:54 -0600

    added password functionality from backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
                         uniqueness: {case_sensitive: false },
                         length: { minimum: 3, maximum: 105},
                         format: { with: VALID_EMAIL_REGEX }
-
+  has_secure_password
 end
